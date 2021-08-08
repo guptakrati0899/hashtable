6a6026d6f35bb9cd575295acc5c494edf30a355d
KratiGupta <kratigupta8545@gmail.com>
2021-08-09 00:40:49 +0530

    Added Welcome message successfully

---
+++ MyHashMap.java
@@ -0,0 +1,17 @@
+package hashtable;
+
+import linkedlist.MyLinkedList;
+
+public class MyHashMap<K, V> {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to master branch of hashtable");
+	}
+
+	
+	
+	
+	 
+
+}
