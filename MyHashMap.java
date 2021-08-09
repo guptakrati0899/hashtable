9fd9521eea8ca5281ee14be26a0306818bdcc002
KratiGupta <kratigupta8545@gmail.com>
2021-08-09 17:31:49 +0530

    Added to calculate frequency of large para

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,47 +1,56 @@
 package hashtable;
 
+
 import linkedlist.MyLinkedList;
 
+import java.util.ArrayList;
+
 public class MyHashMap<K, V> {
-	
-	MyLinkedList<K>  myLinkedList;
-	
+	private final int numBuckets;
+	ArrayList<MyLinkedList<K>> myBucketArray;
 	
 	public MyHashMap() {
+		this.numBuckets = 10;
+		this.myBucketArray = new ArrayList<>(numBuckets);
 		
-		this.myLinkedList = new MyLinkedList<>();
-		
+		for (int i = 0; i < numBuckets; i++) {
+			this.myBucketArray.add(null);
+		}
+	}
+	
+	private int getBucketIndex(K key) {
+		int hashCode = Math.abs(key.hashCode());
+		int index = hashCode % numBuckets;
+		return index;
 	}
 
-	public V get(K  key) {
-		
-		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkedList.search(key);
+	public V get(K key) {
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList= this.myBucketArray.get(index);
+		if (myLinkedList == null) {
+			return null;
+		}
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.search(key);
 		return (myMapNode == null) ? null : myMapNode.getValue();
 	}
 
 	public void add(K key, V value) {
-		
-		MyMapNode<K, V> myMapNode = (MyMapNode<K,V>) this.myLinkedList.search(key);
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedListTest = this.myBucketArray.get(index);
+		if (myLinkedListTest == null) {
+			myLinkedListTest = new MyLinkedList<>();
+			this.myBucketArray.set(index, myLinkedListTest);
+		}
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedListTest.search(key);// TODO Auto-generated method stub
 		if (myMapNode == null) {
 			myMapNode = new MyMapNode<>(key, value);
-			this.myLinkedList.append(myMapNode);
-			}
-		
-		else {
+			myLinkedListTest.append(myMapNode);
+		} else {
 			myMapNode.setValue(value);
-			
 		}
-		
-		
 	}
-
-	@Override
+	
 	public String toString() {
-		 return "MyHashMapNodes{" + myLinkedList + '}';
+		return "MyHashNodes List{" + myBucketArray + "}";
 	}
-	
-	
-	
-	 
-
 }
--- MyHashMapTest.java
+++ MyHashMapTest.java
@@ -7,36 +7,32 @@ public class MyHashMapTest {
 
 	@Test
 	public void givenASentence_WhenWordsAreAddedToListShoulReturnWordFrequency() {
-		String sentence = "To be or not to be";
-		MyHashMap<String, Integer> myHashMap = new 	MyHashMap<>();
-		String[] words = sentence.toLowerCase().split(" ");
-		
-		
-		for (String word : words) {
-			
-			Integer value = myHashMap.get(word);
-			if (value == null) {
-			value = 1;
+			String sentence = "Paranoids are not paranoid because they are paranoid but " + 
+					          "because they keep putting themselves deliberately into " + 
+					          "paranoid avoidable situations";
+			MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+			String[] words = sentence.toLowerCase().split(" ");
+			for (String word : words) {
+				Integer value = myHashMap.get(word);
+				if (value == null) {
+					value = 1;
+				}
+				else {
+					value = value + 1;
+				}
+				myHashMap.add(word, value);
 			}
-			else {
-			value = value + 1;
-			}
-			myHashMap.add(word, value);
+			int frequency = myHashMap.get("paranoid");
+			System.out.println(myHashMap);
+			Assert.assertEquals(3, frequency);
 			
-	}	
-		
-	
-		
-		int frequency = myHashMap.get("be");
-		System.out.println(myHashMap);
-		Assert.assertEquals(2,frequency);
-			
-		}
-		
-		
-		
-		
-		
 	}
+}
+		
+		
+		
+		
+		
+
 
 
