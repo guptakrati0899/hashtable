ce2f0c67b25c100ac3a6ca161988ac63749386ce
KratiGupta <kratigupta8545@gmail.com>
2021-08-09 16:25:43 +0530

    Refactored the linked list

--- MyHashMap.java
+++ MyHashMap.java
@@ -4,11 +4,41 @@ import linkedlist.MyLinkedList;
 
 public class MyHashMap<K, V> {
 	
-	public static void main(String[] args) {
+	MyLinkedList<K>  myLinkedList;
+	
+	
+	public MyHashMap() {
 		
-		System.out.println("Welcome to master branch of hashtable");
+		this.myLinkedList = new MyLinkedList<>();
+		
 	}
 
+	public V get(K  key) {
+		
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkedList.search(key);
+		return (myMapNode == null) ? null : myMapNode.getValue();
+	}
+
+	public void add(K key, V value) {
+		
+		MyMapNode<K, V> myMapNode = (MyMapNode<K,V>) this.myLinkedList.search(key);
+		if (myMapNode == null) {
+			myMapNode = new MyMapNode<>(key, value);
+			this.myLinkedList.append(myMapNode);
+			}
+		
+		else {
+			myMapNode.setValue(value);
+			
+		}
+		
+		
+	}
+
+	@Override
+	public String toString() {
+		 return "MyHashMapNodes{" + myLinkedList + '}';
+	}
 	
 	
 	
